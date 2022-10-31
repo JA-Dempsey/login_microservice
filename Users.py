09da4435ab8b1ca8ac7eadf90371cc6ed285af4e
JA-Dempsey <andrew@dempseycentral.com>
2022-10-31 19:14:33 -0400

    Basic Functionality

---
+++ Users.py
@@ -0,0 +1,64 @@
+class Users():
+
+    def __init__(self):
+        """Initializes the User class for the microservice.
+        It tracks current files in a dictionary for now, but can be
+        expanded for larger use cases if needed in the future."""
+        self.users = {}  # Init dict for users
+        self.current_num = 0  # Count of current users logged in
+
+    def add_user(self, user, data=None):
+        """Adds a new user to the list. Can associate
+        any data with the user if wanted, but defaults to None.
+        Do not include a password as a type of data."""
+
+        # Only add user if not already logged in
+        if self.verify_unique(user):
+            print(self.verify_unique(user))
+            self.users[str(user)] = data
+            self.current_num += 1
+
+    def verify_unique(self, user):
+        """Method that returns False if the given name is already in
+        the users dictionary. Otherwise, it will return True."""
+        for value in self.users.keys():
+            print(value)
+            if user == value:
+                return False
+        
+        return True
+
+    def remove_user(self, user, data=None):
+        """Removes a new user from the list. Will return any
+        data associated with it."""
+
+        data = self.users[user] 
+
+        # Remove the user if it exists.
+        result = self.users.pop(user, "No such user exists.")
+
+        return result, data  # Tuple, data will be None if no data was associated
+
+    def return_current(self):
+        return self.current_num
+
+    def update_login_status(self, user):
+        """Method that changes the current login status of a user."""
+        if self.users[user] is True:
+            self.users[user] = False
+            self.current_num -= 1
+        else:
+            self.users[user] = True
+            self.current_num += 1
+
+
+def find_name(json):
+    """Function that the value of the name key in the given json.
+    If there is no name key, the function will return None."""
+    # Find and return the 'name' key in the json
+    for key in json:
+        if key == "name":
+            return json[key]
+
+    # Only returns none if a 'name' key is not found.
+    return None
---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request, redirect, jsonify
+from Users import Users, find_name
+
+app = Flask(__name__)
+
+users = Users()
+
+@app.route('/')
+def index():
+    return redirect('/info')
+
+@app.route('/info')
+def server_info():
+    return render_template('info.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    content_type = request.headers.get('Content-Type')
+    if content_type == 'application/json':
+        json = request.json
+        
+        name = find_name(json)
+        users.add_user(name)
+
+        print(users.return_current(), name)
+
+        return 'User was added.', 204
+
+    else:
+        print("Invalid content_type, must be application/json")
+        return None, 204
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    content_type = request.headers.get('Content-Type')
+    if content_type == 'application/json':
+        json = request.json
+        
+        name = find_name(json)
+        result, Sdata = users.remove_user(name)  # data contains associated data
+
+        return 'User was removed.', 204
+
+    else:
+        print("Invalid content_type, must be application/json")
+        return None
+
+if __name__ == '__main__':
+    app.run(debug=True)
